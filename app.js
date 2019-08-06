0d0228fa6472069dbdbd02daaf4980999b041d4b
whisping <whisping@gmail.com>
2019-08-06 06:30:23 +0400

    some preparation / now logs in console

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
 app.listen(3000, function() {
-    console.log("Server is running on port 3000.");
+    console.log("Server is running on port 3000. Bugaga.");
+});
+
+app.get("/", function(req, res) {
+    console.log("some log");
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    var firstName = req.body.first;
+    var lastName = req.body.last;
+    var email = req.body.email;
+    console.log(firstName + " " + lastName + " " + email);
 });
